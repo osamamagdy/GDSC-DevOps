9429a76c61cc6eb1b375c7e98af42c4d759d4d9f
osamamagdy <osamamagdy174@gmail.com>
2021-10-20 23:26:50 +0200

    dockerizing the application

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const redis = require('redis');
 const app = express();
 
-const client = redis.createClient();
+const client = redis.createClient(
+    {
+        host : "redis-server",
+        port : 6379
+    }
+);
 
 client.set('visits', 0);
 
