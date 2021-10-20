776c12e197b454634bc3f44cb924a88e7e89e96a
Osama Magdy <59124937+osamamagdy@users.noreply.github.com>
2021-10-20 20:31:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,26 @@ const client = redis.createClient();
 client.set('visits', 0);
 
 
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+
+
+var app_port = app.listen(process.env.PORT || 8080);
+
+
+app.listen(app_port,()=>{
+    console.log("Listening on port 8080");
+});
+
+
 app.get('/',(req,res)=>{
     //This will be executed with every request to the node server
     client.get('visits', (err, visits)=>{
-        res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits)+1);
+        res.render('home', {  style:"home"  , visits: visits});
         //Increase the number of visitors by 1
-    })
-});
-
-app.listen(8080,()=>{
-    console.log("Listening on port 8080");
+    });
 });
